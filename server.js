72ff75c17f7c2371064009ea75ec2d94090fdb6f
Gizzmo614 <genirals666@gmail.com>
2025-06-26 17:10:31 +0700

    Удалён дублирующий импорт notifyAllUsers для устранения ошибки SyntaxError

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const { notifyAllUsers } = require('./bot');
 const { Telegraf } = require('telegraf');
 require('dotenv').config();
 const fs = require('fs');
